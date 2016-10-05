8686b5b837b41b8cf6c89f52c8bf6e631af88e44
qinghao1 <qinghao1+github@gmail.com>
2016-10-05 16:52:36 +0800

    changed

---
+++ helper.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+var ev3 = require('./node_modules/ev3source/ev3.js');
+var source = require('./node_modules/ev3source/source.js');
+
+var motorA = ev3.motorA();
+var motorB = ev3.motorB();
+
+//HELPER FUNCTIONS
+
+function go_forward(distance) { //distance is in cm
+	ev3.runForTime(motorA, 1, distance * 24);
+	ev3.runForTime(motorB, 1, distance * 24);
+}
+
+function turn_left(degree) {
+	ev3.runForDistance(motorA, -1000, degree / 90 * 235);
+	ev3.runForDistance(motorB, 1000, degree / 90 * 235);
+}
+
+function turn_right(degree) {
+	ev3.runForDistance(motorA, 1000, degree / 90 * 235);
+	ev3.runForDistance(motorB, -1000, degree / 90 * 235);
+}
+
+var light_threshold = 50; //TO BE CONFIRMED
+
+function at_line() {
+	return ev3.reflectedLightIntensity(colorSensor) < light_threshold;
+}
+
+var timeout = 1000; //timeout in ms
